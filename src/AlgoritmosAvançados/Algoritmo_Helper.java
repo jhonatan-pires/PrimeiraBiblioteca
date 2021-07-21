54f9fd20f07f5ad47746ff3738a67d65bc3caad9
jhonatan <jhonatanpires636@gmail.com>
2021-07-21 08:00:37 -0300

    Estudando e aprendendo

--- src/AlgoritmosAvançados/Algoritmo_Helper.java
+++ src/AlgoritmosAvançados/Algoritmo_Helper.java
@@ -4,21 +4,26 @@ public class Algoritmo_Helper {
     public static void main(String[] args) {
         ListaAlunos_Factory_Aux listaAlunos_factory_aux = new ListaAlunos_Factory_Aux();
 
-        Aluno_Factory aluno_Factory = new Aluno_Factory();
-        aluno_Factory.nome = "Jhonatan";
-        listaAlunos_factory_aux.adicionar(aluno_Factory);
+        Aluno_Factory aluno_Factory0 = new Aluno_Factory();
+        aluno_Factory0.nome = "Jhonatan";
+        listaAlunos_factory_aux.adicionar(aluno_Factory0);
 
-        aluno_Factory = new Aluno_Factory();
-        aluno_Factory.nome = "Alves";
-        listaAlunos_factory_aux.adicionar(aluno_Factory);
+        Aluno_Factory aluno_Factory1 = new Aluno_Factory();
+        aluno_Factory1.nome = "Alves";
+        listaAlunos_factory_aux.adicionar(aluno_Factory1);
 
-        aluno_Factory = new Aluno_Factory();
-        aluno_Factory.nome = "Pires";
-        listaAlunos_factory_aux.adicionar(aluno_Factory);
-        
-        for (int i = 0; i < listaAlunos_factory_aux.tamanhoLis(); i++){
+        Aluno_Factory aluno_Factory2 = new Aluno_Factory();
+        aluno_Factory2.nome = "Pires";
+        listaAlunos_factory_aux.adicionar(null);
+
+        for (int i = 0; i < listaAlunos_factory_aux.tamanhoLis(); i++) {
             Aluno_Factory a = listaAlunos_factory_aux.obter(i);
-            System.out.println("Nome: " + a.nome);
+            if (a != null) {
+                System.out.println("Nome: " + a.nome);
+            }else {
+                System.out.println("Aluno sem nome:  ");
+            }
         }
+        listaAlunos_factory_aux.remover(aluno_Factory2);
     }
 }
--- src/AlgoritmosAvançados/ListaAlunos_Factory_Aux.java
+++ src/AlgoritmosAvançados/ListaAlunos_Factory_Aux.java
@@ -15,7 +15,6 @@ public class ListaAlunos_Factory_Aux {
     void adicionar(Aluno_Factory aluno_Factory){
         if(tamanho == lista.length){
             Aluno_Factory[] novaList = new Aluno_Factory[QUANTIDADE_LISTAS + lista.length];
-
             for (int i = 0; i < lista.length; i++){
                 novaList[i] = lista [i];
             }
@@ -24,6 +23,27 @@ public class ListaAlunos_Factory_Aux {
         lista[tamanho] = aluno_Factory;
         tamanho++;
     }
+    void remover(Aluno_Factory aluno_Factory) {
+        int i;
+        for (i = 0; i < tamanho; i++) ;
+        Aluno_Factory a = lista[i];
 
+        if (a != null && a.equals(aluno_Factory)) {
+            remover(i);
+
+        } else if (a == null && aluno_Factory == null) {
+            remover(i);
+        }
+    }
+
+    void remover(int indice){
+        int indiceInicial = indice + 1;
+        
+        for(int i = indiceInicial; i < tamanho; i++){
+            lista[i - 1] = lista[i];
+        }
+        tamanho--;
+        lista[tamanho] = null;
+    }
 
 }
